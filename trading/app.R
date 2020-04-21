ebb0be32b666be52bcd86a4658bf6a08f70d4c70
Andre Matache <av.matache@gmail.com>
2020-04-21 17:43:56 -0500

    macd hist signal strat employed v1

--- trading/app.R
+++ trading/app.R
@@ -8,6 +8,7 @@
 # Start building a predictive model and check assumptions
 
 library(shiny)
+library("shinyWidgets")
 library("quantmod")
 library(tidyverse)
 library(tidyquant)
@@ -18,140 +19,80 @@ library("RcppRoll")
 library(jsonlite)
 library(data.table)
 library(zoo)
+library(plotly)
+
+library("BatchGetSymbols")
 
 options(scipen=999)
 
-horizon <- "DAILY"
-#"INTRADAY"
-symbol <- "DIA"
-interval <- ""
-  #"&interval=60min"
-#only for intraday, allowable: 1-5-15-30-60min
+Sys.setenv(TZ='EST')
 
-scrty <- fromJSON(
-    paste0(
-        "https://www.alphavantage.co/query?function=TIME_SERIES_",
-        horizon,
-        "&symbol=",
-        symbol,
-        interval,
-        "&outputsize=full&apikey=SEZGQTSOZXF24IBJ"
-        )
-    ) %>% 
-    .[[2]] 
-df <- data.frame(as.POSIXct(names(scrty),tz="America/New_York"),matrix(unlist(scrty), nrow=length(scrty), byrow=T),stringsAsFactors = FALSE) %>% 
-    mutate_if(is.character,as.numeric) %>% 
-    arrange(.[[1]]) 
+#horizon <- "DAILY"
+##"INTRADAY"
+#symbol <- "TSLA"
+#interval <- ""
+#  #"&interval=60min"
+##only for intraday, allowable: 1-5-15-30-60min
+#
+#scrty <- fromJSON(
+#    paste0(
+#        "https://www.alphavantage.co/query?function=TIME_SERIES_",
+#        horizon,
+#        "&symbol=",
+#        symbol,
+#        interval,
+#        "&outputsize=full&apikey=SEZGQTSOZXF24IBJ"
+#        )
+#    ) %>% 
+#    .[[2]] 
+#df <- data.frame(as.POSIXct(names(scrty),tz="America/New_York"),matrix(unlist(scrty), nrow=length(scrty), byrow=T),stringsAsFactors = FALSE) %>% 
+#    mutate_if(is.character,as.numeric) %>% 
+#    arrange(.[[1]]) 
+#
+#colnames(df) <- c("date","open","high","low","close","vol")
 
-colnames(df) <- c("date","open","high","low","close","vol")
+ticks <- c("TSLA","GILD","GRVY")#,"FXI","SH","TTWO","BP","IMLFF","SPXS","ERY","SMDD","SDOW")
 
-#test moving average theory
-m <- 12
-n <- 26
-p <- 9
-a <- 2/(m+1)
-l <- 1
+
+market <- GetSP500Stocks()  
+market %>% distinct(GICS.Sector) %>% .[[1]] -> sectors 
+market %>% distinct(GICS.Sub.Industry) %>% .[[1]] -> industries 
+
+tickers <- unique(c(ticks,market$Tickers))
+
 
 #add buy/sell signals strat: https://www.dailyfx.com/forex/education/trading_tips/daily_trading_lesson/2020/01/09/macd-histogram.html
 ## strategy: check last 5 slope and if we are increasing (decreasing) and max (min) value for buy (sell)
-last_n <- 5
-
-df <- df %>% 
-    #arrange(desc(date)) %>% 
-    #slice(1:(100)) %>% 
-    arrange(date) %>% 
-    #simple moving averages
-    mutate(sma_m=roll_sum(lag(close,l),m, fill=NA, align="right")/m,
-           sma_n=roll_sum(lag(close,l),n, fill=NA, align="right")/n,
-           macd=sma_m-sma_n
-           ) %>% 
-    #exponential moving averages + macd
-    mutate(ema_m=EMA(close,m),
-           ema_n=EMA(close,n),#a*close+(1-a)*sma_n
-           macd=ema_m-ema_n
-           ) %>% 
-    # signal + histogram    
-    mutate(signal=EMA(macd,p),
-           hist=macd-signal
-    ) %>% 
-    drop_na() %>% 
-    mutate(hist_n=(hist-min(hist))/(max(hist)-min(hist)), #Normalization - consider standardization instead
-           cross_0=if_else(lag(hist)<=0&hist>=0|lag(hist)>=0&hist<=0,1,0),
-           cross_w=if_else(as.numeric(date-lag(date))>1,1,0),
-           cross=if_else(cross_0==1|cross_w==1,1,0)
-           ) %>%  
-    drop_na() %>% 
-## Hist Signal Analysis ##      
-  mutate(
-      #hwm=roll_min(abs(hist),last_n,fill=NA,align="right"),
-      #slope=(hist-lag(hist,last_n-1))/last_n,
-      interval=cumsum(cross)
-    ) %>% 
-  group_by(interval) %>% 
-  mutate(
-    pos=row_number(),
-    test=if_else(cross==0,
-      if_else(abs(hist)>=abs(lag(hist)),1,0),
-      1
-    ),
-    test=cumsum(test)
-  ) %>% 
-  mutate(
-    hwm=rollapply(abs(hist)#if_else(test==pos,abs(hist),0)
-                  ,pos,FUN=max,na.rm = F, align="right")
-  ) %>% 
-  mutate(
-    signal=if_else(cross==0,if_else(abs(hist)<abs(lag(hist))&abs(lag(hist))==hwm
-                   ,if_else(hist<=0,"buy","sell")
-                   ,"neutral"
-    ),"cross")
-  ) %>% 
-  drop_na() 
-  
- 
-  ggplot(df %>% filter(year(date)==2020),aes(x=date)) +
-    geom_bar(stat="identity",aes(y=hist,fill=signal)) + 
-    geom_line(aes(y=close/10)) +
-    geom_point(data=df %>% filter(year(date)==2020,signal!="neutral"),aes(y=close/10,color=signal)) +
-    scale_y_continuous(
-      
-      # Features of the first axis
-      name = "hist",
-      
-      # Add a second axis and specify its features
-      sec.axis = sec_axis( trans=~.*10, name="MACD Histogram")
-    ) 
-
-
+#last_n <- 5
 
 
 ## Sustained Return Trend Analysis ##
 
-y <- 2020
-per <- 12 #5 hours or 5 days of sustained increase/decrease
-del <- .03 #delta cutoff, e.g. 3% daily swings
-
-algo <- df %>% 
-    filter(year(date)==y) %>% 
-    mutate(delta=close/lag(close)-1) %>% 
-    drop_na() 
-#algo %>% filter(delta>0)
-
-#cutoff .375 and .625???
-leads <- shift(algo$delta,n=1:per, type = "lead")
-return <- leads[[1]] + 1
-for (i in 2:per) {
-  return <- return*(leads[[i]]+1) #multiply first element of each list to each other, e.g. leads1-5
-}
-
-algo$return <- return-1
-algo <- algo %>% 
-    #mutate(upswing=as.numeric(per==roll_sum(lead(delta)>0,per, fill=NA, align="left"))) %>% #check for sustained positive trend too
-    drop_na()
-
-ggplot(algo,aes(x=hist_n,y=return)) +
-    geom_point() +
-  geom_smooth()
+#y <- 2020
+#per <- 12 #5 hours or 5 days of sustained increase/decrease
+#del <- .03 #delta cutoff, e.g. 3% daily swings
+#
+#algo <- df %>% 
+#    filter(year(date)==y) %>% 
+#    mutate(delta=close/lag(close)-1) %>% 
+#    drop_na() 
+##algo %>% filter(delta>0)
+#
+##cutoff .375 and .625???
+#leads <- shift(algo$delta,n=1:per, type = "lead")
+#return <- leads[[1]] + 1
+#for (i in 2:per) {
+#  return <- return*(leads[[i]]+1) #multiply first element of each list to each other, e.g. leads1-5
+#}
+#
+#algo$return <- return-1
+#algo <- algo %>% 
+#    #mutate(upswing=as.numeric(per==roll_sum(lead(delta)>0,per, fill=NA, align="left"))) %>% #check for sustained positive trend too
+#    drop_na()
+#
+#ggplot(algo,aes(x=hist_n,y=return)) +
+#    geom_point() +
+#  geom_smooth()
 
 
 
@@ -213,10 +154,10 @@ ggplot(algo,aes(x=hist_n,y=return)) +
 #chartSeries(GS) 
 
 #should make these user interactive/inputable
-qt <- c("TSLA","GILD","GRVY","FXI","SH","TTWO","BP","IMLFF","SPXS","ERY","SMDD","SDOW")
-cb <- c(265.77,74.37,22.93,34.36,31.61,102.25,39.53,.45,17.02,117.65,10.78,51.67)
+#qt <- c("TSLA","GILD","GRVY","FXI","SH","TTWO","BP","IMLFF","SPXS","ERY","SMDD","SDOW")
+#cb <- c(265.77,74.37,22.93,34.36,31.61,102.25,39.53,.45,17.02,117.65,10.78,51.67)
 
-dat <- data.frame(stock=character(),quote=numeric())
+#dat <- data.frame(stock=character(),quote=numeric())
 
 #for(i in 1:length(qt)) {
 #
@@ -237,47 +178,211 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            actionButton('rfrsh','Refresh')
+            #actionButton('rfrsh','Refresh')
+          numericInput("lookback","Select Historical Horizon",value=120,min=120,max=120),
+          selectInput("freq","Select Quote Frequency",choices='daily',selected='daily'),
+          selectInput("price","Select Quote Type",choices="close",selected="close"),
+          column(12,h5("*Select Time Lengths")),
+          fluidRow(column(3,numericInput("short","ShortTerm",value=12,min=12,max=12)),
+                   column(3,numericInput("long","LongTerm",value=26,min=26,max=26)),
+                   column(3,numericInput("sign","Signal",value=9,min=9,max=9)),
+                   column(3,numericInput("lag","Lag",value=1,min=1,max=1))
+          ),
+          selectInput("sec","Select Quote Sector(s)",choices=sectors
+                      ,selected=sectors
+                      ,multiple=T
+          ),
+          #selectInput("ind","Select Quote Industry(ies)",choices=industries
+          #            ,selected=industries
+          #            ,multiple=T
+          #),
+          selectInput("ticks","Portfolio Stocks",choices=tickers
+                      ,selected=ticks
+                      ,multiple=T
+          )
         ),
 
         # Show a plot of the generated distribution
-        mainPanel(
-           dataTableOutput("quotes") %>% withSpinner(color="#1A4C64")
+        mainPanel(textOutput("test"),
+           h3("Quotes: Sell, Buy, ?"),
+           dataTableOutput("sell") %>% withSpinner(color="#1A4C64"),
+           dataTableOutput("buy") %>% withSpinner(color="#1A4C64"),
+           dataTableOutput("tbd") %>% withSpinner(color="#1A4C64"),
+           h3("Portfolio"),
+           plotlyOutput("plot") %>% withSpinner(color="#1A4C64")
         )
     )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
 
-    tbl <- eventReactive(input$rfrsh,{
-        tbl <- dat
-        for(i in 1:length(qt)) {
-            
-            html <- paste0("https://finance.yahoo.com/quote/",qt[i],"?p=",qt[i],"&.tsrc=fin-srch") %>% 
-                read_html() %>% 
-                html_nodes(xpath = "/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[4]/div/div/div/div[3]/div[1]/div/span[1]") %>% 
-                html_text()
-            tbl <- tbl %>% 
-                bind_rows(data.frame(stock=qt[i],quote=as.numeric(html)))
-        }
-        tbl
+   #tbl <- eventReactive(input$rfrsh,{
+   #    tbl <- dat
+   #    for(i in 1:length(qt)) {
+   #        
+   #        html <- paste0("https://finance.yahoo.com/quote/",qt[i],"?p=",qt[i],"&.tsrc=fin-srch") %>% 
+   #            read_html() %>% 
+   #            html_nodes(xpath = "/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[4]/div/div/div/div[3]/div[1]/div/span[1]") %>% 
+   #            html_text()
+   #        tbl <- tbl %>% 
+   #            bind_rows(data.frame(stock=qt[i],quote=as.numeric(html)))
+   #    }
+   #    tbl
+   #})
+    
+  #observeEvent(input$sec,{
+  #  market %>% 
+  #    filter(GICS.Sector %in% input$sec) %>%  
+  #    distinct(GICS.Sub.Industry) %>% 
+  #    .[[1]] -> choices
+  #  
+  #  updateSelectInput(session,"ind",choices=choices,selected=choices)
+  #  
+  #})
+  
+  dat <- reactive({
+    
+    market %>% 
+      filter(GICS.Sector %in% input$sec#,GICS.Sub.Industry %in% input$ind
+             ) %>%  
+      distinct(Tickers) %>% 
+      .[[1]]-> choices 
+    
+    tickers <- unique(c(input$ticks,choices))
+    
+    end <- Sys.Date() + 1 #check on this!!!
+    str <- end - input$lookback
+    price <- input$price
+    freq <- input$freq
+    #test moving average theory
+    m <- input$short
+    n <- input$long
+    p <- input$sign
+    a <- 2/(m+1)
+    l <- input$lag
+    withProgress(message = 'Gathering Quote Details',
+                 detail = 'This may take a while...', value = 0, {
+
+    l.out <- BatchGetSymbols(tickers = tickers, 
+                             first.date = str,
+                             last.date = end, 
+                             freq.data = freq) 
+
+    incProgress(1, detail = "Query Complete")
+                
     })
     
+    df <- l.out$df.tickers %>% 
+     select(ticker,ref.date, contains(price), everything()) %>% 
+     rename(price=3) %>% 
+     arrange(ticker,ref.date) %>% 
+     group_by(ticker) %>%
+     #simple moving averages
+     mutate(sma_m=roll_sum(lag(price,l),m, fill=NA, align="right")/m,
+            sma_n=roll_sum(lag(price,l),n, fill=NA, align="right")/n,
+            macd=sma_m-sma_n
+     ) %>% 
+     #exponential moving averages + macd
+     mutate(ema_m=EMA(price,m),
+            ema_n=EMA(price,n),#a*close+(1-a)*sma_n
+            macd=ema_m-ema_n
+     ) %>% 
+     # signal + histogram    
+     mutate(signal=EMA(macd,p),
+            hist=macd-signal
+     ) %>% 
+     drop_na() %>% 
+     mutate(hist_n=(hist-min(hist))/(max(hist)-min(hist)), #Normalization - consider standardization instead
+            cross_0=if_else(lag(hist)<=0&hist>=0|lag(hist)>=0&hist<=0,1,0),
+            cross_w=if_else(as.numeric(ref.date-lag(ref.date))>1,1,0),
+            cross=if_else(cross_0==1|cross_w==1,1,0)
+     ) %>%  
+     drop_na() %>% 
+     ## Hist Signal Analysis ##      
+     mutate(
+       #hwm=roll_min(abs(hist),last_n,fill=NA,align="right"),
+       #slope=(hist-lag(hist,last_n-1))/last_n,
+       interval=cumsum(cross)
+     ) %>% 
+     group_by(ticker,interval) %>% 
+     mutate(
+       hwm=rollapply(abs(hist)#if_else(test==row_number(),abs(hist),0)
+                     ,row_number(),FUN=max,na.rm = F, align="right")
+     ) %>% 
+     mutate(
+       signal=if_else(cross==0,if_else(abs(hist)<abs(lag(hist))&abs(lag(hist))==hwm
+                                       ,if_else(hist<=0,"buy","sell")
+                                       ,"neutral"
+       ),"cross")
+     ) %>% 
+     drop_na() 
     
-    output$quotes <- renderDataTable({
-        tbl <- tbl() %>% 
-            mutate(cost=cb) %>% 
-            mutate(diff=round(quote-cost,2),flag=if_else(diff<0,1,0))
+    df
+    
+  })
+  
+  #output$test <- renderText({as.character(
+  #  max(dat()$ref.date)
+  #)})
+  
+  output$sell <- renderDataTable({
+
+    datatable(dat() %>% filter(signal=="sell",ref.date==Sys.Date()))
+    
+  })
+  output$buy <- renderDataTable({
+    
+    datatable(dat() %>% filter(signal=="buy",ref.date==Sys.Date()))
+    
+  })
+  output$tbd <- renderDataTable({
+    
+    datatable(dat() %>% filter(signal=="cross",ref.date==Sys.Date()))
+    
+  })
+  
+  output$plot <- renderPlotly({
+    
+    df <- dat()
+    
+    gplot <- ggplot(df %>% filter(ticker %in% input$ticks),aes(x=ref.date)) +
+      geom_line(aes(y=price)) +
+      geom_point(data=df %>% filter(ticker %in% input$ticks,signal!="neutral"),aes(y=price,color=signal#,size=volume
+      )) +
+      geom_bar(stat="identity",aes(y=hist,fill=signal)) + 
+      scale_y_continuous(
         
-        datatable(tbl, options=list(columnDefs = list(list(visible=FALSE, targets=5)))
-                  ) %>% 
-            formatStyle(
-                'diff', 'flag',
-                color = styleEqual(1, 'red')
-            )
-    })
+        # Features of the first axis
+        name = "Close",
+        
+        # Add a second axis and specify its features
+        sec.axis = sec_axis( trans=~., name="MACD Histogram")
+      ) +
+      facet_wrap(vars(ticker),scales = 'free_y') +
+      theme_minimal()
+    
+    ggplotly(gplot)
+    
+  })
+    
+   #output$quotes <- renderDataTable({
+   #    tbl <- tbl() %>% 
+   #        mutate(cost=cb) %>% 
+   #        mutate(diff=round(quote-cost,2),flag=if_else(diff<0,1,0))
+   #    
+   #    datatable(tbl, options=list(columnDefs = list(list(visible=FALSE, targets=5)))
+   #              ) %>% 
+   #        formatStyle(
+   #            'diff', 'flag',
+   #            color = styleEqual(1, 'red')
+   #        )
+   #})
+  
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+#library(rsconnect)
+#rsconnect::deployApp("/home/andr31/R/github/stock/trading/")
